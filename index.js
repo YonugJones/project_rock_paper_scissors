722e942f94a34061a4b8e35206c611d446efaaa7
YonugJones <peterckerfoot@gmail.com>
2023-06-15 17:49:28 -0500

    computer given random selector for game player given prompt for entering selector result of single round game displayed

---
+++ index.js
@@ -0,0 +1,24 @@
+ function getComputerChoice () {
+    const computerPick = ['rock', 'paper', 'scissors'];
+        return computerPick[Math.floor(Math.random() * computerPick.length)];
+ }
+
+ function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return `It's a tie! You both chose ${playerSelection}`;
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "You won! Your paper COVERED the rock!";
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "You won! Your rock SMASHED the scissors";
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "You won! Your scissors SLICED the paper";
+    } else {
+        return `YOU LOST! ${computerSelection} BEATS ${playerSelection}`;
+    }
+  }
+   
+  const playerSelection = prompt('Choose your weapon: "rock", "paper", or "scissors".').toLowerCase();
+  const computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
+
+
